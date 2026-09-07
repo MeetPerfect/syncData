238e798a6f12a4173b21882e2c74dd3a9f6da1f6
MeetPerfect <649083275@qq.com>
2026-09-07 15:59:49 +0800

    feat: 增加切片分块批处理插入数据

---
+++ utils/chunk.go
@@ -0,0 +1,13 @@
+package utils
+
+func Chunk[T any](items []T, size int) [][]T {
+	var chunks [][]T
+	for i := 0; i < len(items); i += size {
+		end := i + size
+		if end > len(items) {
+			end = len(items)
+		}
+		chunks = append(chunks, items[i:end])
+	}
+	return chunks
+}
